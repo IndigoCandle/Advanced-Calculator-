136415e97b443c50870ec3e38f19062b2c386341
IndigoCandle <romyannay1@gmail.com>
2023-12-28 15:05:15 +0200

    minus handling

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def convert_str_to_lst(expression: str):
     output = []
     temp_string = ''
     counter = -1
-    while counter < len(expression)-1:
+    while counter < len(expression) - 1:
         temp_string = ''
         if counter >= len(expression) - 1:
             break
@@ -23,19 +23,41 @@ def convert_str_to_lst(expression: str):
 
         if temp_string != '':
             output.append(temp_string)
-            if counter < len(expression):
-                character = expression[counter]
-                output.append(character)
+        if counter < len(expression):
+            character = expression[counter]
+            output.append(character)
 
     return output
 
 
-def calculator(operation: str):
+def handle_minus(expression_array: list):
+    counter = 0
+    temp_expression_array = expression_array
+    while counter < len(temp_expression_array) - 1:
+        minus_counter = 0
+        while temp_expression_array[counter] == '-':
+            temp_expression_array.pop(counter)
+            minus_counter += 1
+        if minus_counter > 0:
+            if minus_counter % 2 == 0:
+                temp_expression_array.insert(counter, '+')
+            else:
+                temp_expression_array.insert(counter, '-')
+        counter += 1
+    return temp_expression_array
+
+
+def calculator(revised_list: list):
+    dict_kdimut = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "%": 4, "@": 5, "$": 5, "&": 5, "~": 6, "!": 6}
     operands = {"+", "*", "/", "^", "%", "@", "$", "&", "~", "!", "-"}
-    for operand in operands:
+    output = []
+
+    for operand in revised_list:
         operation_lst = operation
 
     return operation_lst
 
 
-print(convert_str_to_lst("59+33*9"))
+data = convert_str_to_lst("59+33-----97")
+print(data)
+print(handle_minus(data))
